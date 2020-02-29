93fc0b07b966668c8fb4cb4a36583115e65d2b1b
david <davidscandurra@gmail.com>
2020-02-29 15:34:25 +0100

    Store model data in vaos

--- src/main/java/team/gutterteam123/helios/model/Model.java
+++ src/main/java/team/gutterteam123/helios/model/Model.java
@@ -1,12 +1,17 @@
 package team.gutterteam123.helios.model;
 
+import lombok.SneakyThrows;
 import org.joml.Vector3f;
 import org.joml.Vector3i;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL20;
+import team.gutterteam123.helios.render.VAO;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
+
+import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
+import static org.lwjgl.opengl.GL11.glDrawArrays;
 
 public class Model {
 
@@ -16,7 +21,10 @@ public class Model {
     /* The triangles - not coordinates but indexes of the vertices array */
     private ArrayList<Vector3i> faces = new ArrayList<>();
 
-    public Model(File file) throws IOException {
+    private VAO vao = new VAO();
+
+    @SneakyThrows
+    public Model(File file) {
         ModelReader reader = new ModelReader(file);
         String command;
         while ((command = reader.nextCommand()) != null) {
@@ -36,6 +44,32 @@ public class Model {
                     System.out.println("Unknown command: " + command);
             }
         }
+        vao.bind();
+        /*vao.store(0, new float[]{
+                -0.5f, -0.5f, 0.0f, // left
+                0.5f, -0.5f, 0.0f, // right
+                0.0f,  0.5f, 0.0f  // top
+        }, 3);*/
+        vao.store(0, getRawData(), 3);
+        vao.unbind();
+    }
+
+    public float[] getRawData() {
+        float[] data = new float[faces.size() * 3 * 3];
+        for (int i = 0; i < faces.size(); i++) {
+            Vector3i face = faces.get(i);
+            int start = i * 3 * 3;
+            saveVertex(data, getVertex(face.x), start);
+            saveVertex(data, getVertex(face.y), start + 3);
+            saveVertex(data, getVertex(face.z), start + 6);
+        }
+        return data;
+    }
+
+    private void saveVertex(float[] data, Vector3f vertex, int start) {
+        data[start] = vertex.x;
+        data[start + 1] = vertex.y;
+        data[start + 2] = vertex.z;
     }
 
     /* Starting at 1 */
@@ -47,18 +81,11 @@ public class Model {
         GL11.glPolygonMode(GL11.GL_FRONT_AND_BACK, GL11.GL_FILL);
         //GL11.glPolygonMode(GL11.GL_FRONT_AND_BACK, GL11.GL_LINE);
 
-        GL11.glBegin(GL11.GL_TRIANGLES);
-        for (Vector3i face : faces) {
-            Vector3f one = getVertex(face.x);
-            GL11.glVertex3f(one.x, one.y, one.z);
-
-            Vector3f two = getVertex(face.y);
-            GL11.glVertex3f(two.x, two.y, two.z);
-
-            Vector3f three = getVertex(face.z);
-            GL11.glVertex3f(three.x, three.y, three.z);
-        }
-        GL11.glEnd();
+        vao.bind();
+        GL20.glEnableVertexAttribArray(0);
+        glDrawArrays(GL_TRIANGLES, 0, 3 * faces.size());
+        GL20.glDisableVertexAttribArray(0);
+        vao.unbind();
     }
 
 }
---
+++ src/main/java/team/gutterteam123/helios/render/VAO.java
@@ -0,0 +1,68 @@
+package team.gutterteam123.helios.render;
+
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL15;
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.opengl.GL30;
+
+import java.util.ArrayList;
+
+/*
+ * A vao can store data directly in the GPU
+ * The Vao stores VBOs (as attributes) witch then store the actual data
+ */
+public class VAO {
+
+    private static ArrayList<VAO> vaos = new ArrayList<>();
+
+    public static void cleanAll() {
+        vaos.forEach(vao -> vao.cleanup(false));
+        vaos.clear();
+    }
+
+    private int vaoID;
+    private ArrayList<Integer> vbos = new ArrayList<>();
+
+    public VAO() {
+        vaoID = GL30.glGenVertexArrays();
+        vaos.add(this);
+    }
+
+    /*
+     * Creates a vbo and adds it to this vao
+     * The VAO needs to be bound first
+     */
+    public void store(int attributeIndex, float[] data, int bundleSize, int usage) {
+        int vboID = GL15.glGenBuffers();
+        vbos.add(vboID);
+
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboID);
+        GL15.glBufferData(GL15.GL_ARRAY_BUFFER, data, usage);
+        GL20.glVertexAttribPointer(attributeIndex, bundleSize, GL11.GL_FLOAT, false, 0, 0);
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
+    }
+
+    public void store(int attributeIndex, float[] data, int bundleSize) {
+        store(attributeIndex, data, bundleSize, GL15.GL_STATIC_DRAW);
+    }
+
+    public void bind() {
+        GL30.glBindVertexArray(vaoID);
+    }
+
+    /*
+     * Binding the default VAO will unbind the current one
+     */
+    public void unbind() {
+        GL30.glBindVertexArray(0);
+    }
+
+    public void cleanup(boolean remove) {
+        if (remove) {
+            vaos.remove(this);
+        }
+        GL30.glDeleteVertexArrays(vaoID);
+        GL15.glDeleteBuffers(vbos.stream().mapToInt(value -> value).toArray());
+    }
+
+}
