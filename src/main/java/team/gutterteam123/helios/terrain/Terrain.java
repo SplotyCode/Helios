5441d28c985fd19c2f5cd8c538f064966cb28633
david <davidscandurra@gmail.com>
2020-03-08 15:43:40 +0100

    Trying to implement terrain grid with GL_TRIANGLE_STRIP instead of GL_TRIANGLES

--- src/main/java/team/gutterteam123/helios/terrain/Terrain.java
+++ src/main/java/team/gutterteam123/helios/terrain/Terrain.java
@@ -4,21 +4,16 @@ import lombok.SneakyThrows;
 import org.joml.Vector4f;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL20;
-import org.lwjgl.stb.STBImage;
-import org.lwjgl.system.MemoryStack;
 import team.gutterteam123.helios.Helios;
 import team.gutterteam123.helios.render.VAO;
 
-import java.nio.ByteBuffer;
-import java.nio.IntBuffer;
 import java.util.Random;
-
-import static org.lwjgl.system.MemoryStack.stackPush;
 
 public class Terrain {
 
-    private static final int MAP_SIZE = 100;
-    private static final int INDICES_SIZE = 6 * (MAP_SIZE - 1) * (MAP_SIZE);
+    private static final int MAP_SIZE = 3;
+    private static final int INDICES_SIZE = (MAP_SIZE * 2 + 2) * MAP_SIZE + ((MAP_SIZE - 1) * 2);
+    private static final int VERTICES_SIZE = 3 * (MAP_SIZE + 1) * (MAP_SIZE + 1);
 
     private TerrainShader shader = new TerrainShader();
 
@@ -36,31 +31,30 @@ public class Terrain {
 
     @SneakyThrows
     private void createMesh() {
-        float[] vertices = new float[MAP_SIZE * MAP_SIZE * 3];
+        float[] vertices = new float[VERTICES_SIZE];
         int[] indices = new int[INDICES_SIZE];
 
         int vertexPointer = 0;
-        for (int i = 0; i < MAP_SIZE; i++) {
-            for (int j = 0; j < MAP_SIZE; j++) {
-                vertices[vertexPointer * 3] = i;
-                vertices[vertexPointer * 3 + 1] = (float) heightGenerator.noise(i / 20f, j / 20f) * 12;
-                vertices[vertexPointer * 3 + 2] = j;
+        for (int z = 0; z < MAP_SIZE + 1; z++) {
+            for (int x = 0; x < MAP_SIZE + 1; x++) {
+                System.out.println(vertexPointer + " " + x + " " + z);
+                vertices[vertexPointer * 3] = x;
+                vertices[vertexPointer * 3 + 1] = 0;//(float) heightGenerator.noise(z / 20f, x / 20f) * 12;
+                vertices[vertexPointer * 3 + 2] = z;
                 vertexPointer++;
             }
         }
+        System.out.println("a " + (vertexPointer - 1) + " " + VERTICES_SIZE / 3);
         int pointer = 0;
-        for (int gz = 0; gz < MAP_SIZE - 1; gz++) {
-            for (int gx = 0; gx < MAP_SIZE - 1; gx++) {
-                int topLeft = (gz * MAP_SIZE) + gx;
-                int topRight = topLeft + 1;
-                int bottomLeft = ((gz + 1) * MAP_SIZE) + gx;
-                int bottomRight = bottomLeft + 1;
+        for (int z = 0; z < MAP_SIZE; z++) {
+            for (int x = 0; x < MAP_SIZE + 1; x++) {
+                int topLeft = (z * MAP_SIZE + 1) + x;
                 indices[pointer++] = topLeft;
-                indices[pointer++] = bottomLeft;
-                indices[pointer++] = topRight;
-                indices[pointer++] = topRight;
-                indices[pointer++] = bottomLeft;
-                indices[pointer++] = bottomRight;
+                indices[pointer++] = topLeft + MAP_SIZE + 1;
+            }
+            if(z < MAP_SIZE - 1) {
+                indices[pointer++] = (z + 1) * (MAP_SIZE + 1) + (MAP_SIZE);
+                indices[pointer++] = (z + 1) * (MAP_SIZE + 1);
             }
         }
         vao.bind();
@@ -76,7 +70,7 @@ public class Terrain {
         vao.bind();
         GL20.glEnableVertexAttribArray(0);
         GL11.glPolygonMode(GL11.GL_FRONT_AND_BACK, GL11.GL_LINE);
-        GL11.glDrawElements(GL11.GL_TRIANGLES, INDICES_SIZE, GL11.GL_UNSIGNED_INT, 0);
+        GL11.glDrawElements(GL11.GL_TRIANGLE_STRIP, INDICES_SIZE, GL11.GL_UNSIGNED_INT, 0);
         GL20.glDisableVertexAttribArray(0);
         vao.unbind();
         shader.stop();
